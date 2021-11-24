10c3ac603d3228d48d6090c83cb76345d5872952
Louis Xu <lxu@F2N018.42adel.org.au>
2021-11-24 17:54:59 +1030

    Fix to satisfy norminette

--- c00-dev/ex02/ft_print_reverse_alphabet.c
+++ c00-dev/ex02/ft_print_reverse_alphabet.c
@@ -1,17 +1,15 @@
 #include <unistd.h>
 
-void ft_print_reverse_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
-	char a;
-	char z;
+	char	a;
+	char	z;
 
 	a = 'a';
 	z = 'z';
-
 	while (z >= a)
 	{
 		write(1, &z, 1);
 		z--;
 	}
 }
-
