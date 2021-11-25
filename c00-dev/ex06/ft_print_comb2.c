d716a14ef4e7ffd205d38fbd19849c2cf14a84a1
Louis Xu <lxu@F2N018.42adel.org.au>
2021-11-25 18:12:55 +1030

    Iterative solution, satisfies norminette

---
+++ c00-dev/ex06/ft_print_comb2.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 16:43:30 by lxu               #+#    #+#             */
+/*   Updated: 2021/11/25 18:10:27 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+/* Note. Watch the endian-ness here. The 'one' column is stored in
+ * column [1]. While the 'tens' is stored in column [0]
+ * ie, the endianness is reversed to make the printing easier */
+void	increment_number(char *ptr_num)
+{	
+	if (ptr_num[1] < '9')
+	{
+		ptr_num[1]++;
+	}
+	else
+	{
+		ptr_num[1] = '0';
+		ptr_num[0]++;
+	}
+}
+
+void	print_number(char *big_num, char *small_num)
+{
+	write(1, big_num, 2);
+	write(1, " ", 1);
+	write(1, small_num, 2);
+	if (! (big_num[0] >= '9' && big_num[1] >= '8' && \
+		small_num[0] >= '9' && small_num[1] >= '9'))
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	char	small_num[2];
+	char	big_num[2];
+
+	small_num[0] = '0';
+	small_num[1] = '0';
+	big_num[0] = '0';
+	big_num[1] = '0';
+	while ((big_num[0] < '9') || (big_num[0] <= '9' && big_num[1] <= '8'))
+	{
+		small_num[0] = big_num[0];
+		small_num[1] = big_num[1];
+		increment_number(small_num);
+		while (small_num[0] <= '9')
+		{
+			print_number(big_num, small_num);
+			increment_number(small_num);
+		}
+		increment_number(big_num);
+	}
+}
