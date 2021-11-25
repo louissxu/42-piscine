5396575ce4d1ac0ca6091d44853a4b0dd30b3f9c
Louis Xu <lxu@F2N018.42adel.org.au>
2021-11-26 00:56:56 +1030

    Fix to satisfy norminette

--- c00-dev/ex08/ft_print_combn.c
+++ c00-dev/ex08/ft_print_combn.c
@@ -1,45 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 00:46:25 by lxu               #+#    #+#             */
+/*   Updated: 2021/11/26 00:56:09 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void increment_digit(char *ijk, int position, int n)
+void	increment_digit(char *ijk, int position, int n)
 {
 	ijk[position] += 1;
 	if (ijk[position] > ('9' - (n - position - 1)))
 	{
-		if(position == 0)
+		if (position == 0)
 		{
 			ijk[position] = 'x';
 		}
 		else
 		{
 			increment_digit(ijk, position - 1, n);
-			ijk[position] = ijk[position-1] + 1;
+			ijk[position] = ijk[position - 1] + 1;
 		}
 	}
 }
 
-void increment_number(char *ijk, int n)
+void	increment_number(char *ijk, int n)
 {
-	increment_digit(ijk, n-1, n);
+	increment_digit(ijk, n - 1, n);
 }
 
-
-void ft_print_combn(int n)
+void	ft_print_combn(int n)
 {
-	if (n != 0)	
+	char	ijk[10];
+	int		counter;
+
+	if (n == 0)
+		return ;
+	counter = 0 - 1;
+	while (++counter < n)
+		ijk[counter] = '0' + counter;
+	while (ijk[0] != 'x')
 	{
-		char ijk[n];
-		int counter;
-
-		counter = 0 - 1;
-		while (++counter < n)
-			ijk[counter] = '0' + counter;
-
-		while (ijk[0] != 'x')
-		{
-			write(1, &ijk, sizeof(ijk));	
-			increment_number(ijk, n);
-			if (ijk[0] != 'x')
-				write(1, ", ", 2);
-		}
+		write(1, &ijk, n);
+		increment_number(ijk, n);
+		if (ijk[0] != 'x')
+			write(1, ", ", 2);
 	}
 }
