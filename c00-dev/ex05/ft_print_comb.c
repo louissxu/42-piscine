23092f0ae0d275f574733a0db7be2be30739cf47
Louis Xu <lxu@F2N018.42adel.org.au>
2021-11-25 16:31:16 +1030

    Modify solution to satisfy norminette

--- c00-dev/ex05/ft_print_comb.c
+++ c00-dev/ex05/ft_print_comb.c
@@ -6,41 +6,31 @@
 /*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 20:25:16 by lxu               #+#    #+#             */
-/*   Updated: 2021/11/24 21:27:11 by lxu              ###   ########.fr       */
+/*   Updated: 2021/11/25 16:30:39 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
-	char numbers[10];
-	char zero = '0';
-	char spacer[2];
-	for(int i = 0; i <= 9; i++)
-	{
-		numbers[i] = zero;
-		zero++;
-	}
-	spacer[0] = ',';
-	spacer[1] = ' ';
+	char	ijk[3];
 
-	for(int i = 0; i <= 7; i++)
+	ijk[0] = '0' - 1;
+	while (++ijk[0] <= '7')
 	{
-		for(int j = (i+1); j<=8; j++)
+		ijk[1] = ijk[0] + 1 - 1;
+		while (++ijk[1] <= '8')
 		{
-			for(int k=(j+1); k<=9; k++)
+			ijk[2] = ijk[1] + 1 - 1;
+			while (++ijk[2] <= '9')
 			{
-				write(1, &numbers[i], 1);
-				write(1, &numbers[j], 1);
-				write(1, &numbers[k], 1);
-				if (i != 7)
+				write(1, &ijk, 3);
+				if (ijk[0] != '7')
 				{
-					write(1, &spacer, sizeof(spacer));
+					write(1, ", ", 2);
 				}
 			}
 		}
-   	}
+	}
 }
-
