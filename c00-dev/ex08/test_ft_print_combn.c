1b88278e7ab989408b67226cd61e204667fac96b
Louis Xu <lxu@F2N018.42adel.org.au>
2021-11-26 00:35:11 +1030

    Working solution using modified counting

---
+++ c00-dev/ex08/test_ft_print_combn.c
@@ -0,0 +1,44 @@
+#include "ft_print_combn.c"
+#include <stdio.h>
+
+void ft_print_combn(int n);
+
+int main(void)
+{
+	/*
+	printf("Testing ft_print_combn(0)\n");
+	printf("-------------------------\n");
+	ft_print_combn(0);
+	*/
+	
+	printf("\n");
+	printf("Testing ft_print_combn(1)\n");
+	printf("-------------------------\n");
+	ft_print_combn(1);
+	printf("\n");
+
+	printf("Testing ft_print_combn(2)\n");
+	printf("-------------------------\n");
+	ft_print_combn(2);
+	printf("\n");
+	
+	printf("Testing ft_print_combn(3)\n");
+	printf("-------------------------\n");
+	ft_print_combn(3);
+	printf("\n");
+
+	printf("Testing ft_print_combn(4)\n");
+	printf("-------------------------\n");
+	ft_print_combn(4);
+	printf("\n");
+	
+	printf("Testing ft_print_combn(9)\n");
+	printf("-------------------------\n");
+	ft_print_combn(9);
+	printf("\n"); 
+	
+	printf("Testing ft_print_combn(10)\n");
+	printf("-------------------------\n");
+	ft_print_combn(10);
+	printf("\n"); 
+}
