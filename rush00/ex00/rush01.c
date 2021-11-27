a4a5ef1849989f85b603d606ef18be9f810e8b3f
Louis Xu <louis.s.xu@gmail.com>
2021-11-27 21:14:58 +1030

    Add Rush solution

---
+++ rush00/ex00/rush01.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/27 21:07:13 by lxu               #+#    #+#             */
+/*   Updated: 2021/11/27 21:07:15 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char *c);
+
+void	print_first_line(int x)
+{
+	int	x_current;
+
+	x_current = 0;
+	while (x_current < x)
+	{
+		if (x_current == 0)
+			ft_putchar("/");
+		else if (x_current == x - 1)
+			ft_putchar("\\");
+		else
+			ft_putchar("*");
+	x_current++;
+	}
+}
+
+void	print_middle_line(int x)
+{
+	int	x_current;
+
+	x_current = 0;
+	while (x_current < x)
+	{
+		if (x_current == 0)
+			ft_putchar("*");
+		else if (x_current == x - 1)
+			ft_putchar("*");
+		else
+			ft_putchar(" ");
+	x_current++;
+	}
+}
+
+void	print_last_line(int x)
+{
+	int	x_current;
+
+	x_current = 0;
+	while (x_current < x)
+	{
+		if (x_current == 0)
+			ft_putchar("\\");
+		else if (x_current == x - 1)
+			ft_putchar("/");
+		else
+			ft_putchar("*");
+	x_current++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	y_current;
+
+	y_current = 0;
+	while (y_current < y)
+	{
+		if (y_current == 0)
+			print_first_line(x);
+		else if (y_current == y - 1)
+			print_last_line(x);
+		else
+			print_middle_line(x);
+		ft_putchar("\n");
+		y_current++;
+	}
+}
