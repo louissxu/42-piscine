334cb90d176be80238719dc5e93a35b426d416e5
Louis Xu <louis.s.xu@gmail.com>
2021-11-30 22:40:03 +1030

    Add test

---
+++ c03-dev/ex00/test_ft_strcmp.c
@@ -0,0 +1,22 @@
+#include "ft_strcmp.c"
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("-------------------");
+	printf("Using inbult strcmp\n");
+	printf("Comparing \"Hello\" and \"Hello\": %d\n", strcmp("Hello", "Hello"));	
+	printf("Comparing \"Hello\" and \"World\": %d\n", strcmp("Hello", "World"));
+	printf("Comparing \"123\" and \"1234\": %d\n", strcmp("123", "1234"));
+	printf("Comparing \"456\" and \"123\": %d\n", strcmp("456", "123"));
+	printf("Comparing \"!@#\" and \"123\": %d\n", strcmp("!@#", "123"));
+
+	printf("-------------------");
+	printf("Using ft_strcmp\n");
+	printf("Comparing \"Hello\" and \"Hello\": %d\n", ft_strcmp("Hello", "Hello"));	
+	printf("Comparing \"Hello\" and \"World\": %d\n", ft_strcmp("Hello", "World"));
+	printf("Comparing \"123\" and \"1234\": %d\n", ft_strcmp("123", "1234"));
+	printf("Comparing \"456\" and \"123\": %d\n", ft_strcmp("456", "123"));
+	printf("Comparing \"!@#\" and \"123\": %d\n", ft_strcmp("!@#", "123"));
+}
