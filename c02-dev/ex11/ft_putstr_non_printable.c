d0e3d29526d9c15ccbf18d658eb9f18a94c3f6ae
Louis Xu <louis.s.xu@gmail.com>
2021-11-30 22:27:24 +1030

    Fix: Norm errors

--- c02-dev/ex11/ft_putstr_non_printable.c
+++ c02-dev/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 02:19:24 by lxu               #+#    #+#             */
-/*   Updated: 2021/11/30 22:18:52 by lxu              ###   ########.fr       */
+/*   Updated: 2021/11/30 22:26:58 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,19 @@ int	is_printable(char c)
 
 char	int_to_hex_char(int num)
 {
-	char *hex_chars;
+	char	*hex_chars;
 
 	hex_chars = "0123456789abcdef";
-	return (hex_chars[num]);	
+	return (hex_chars[num]);
 }
 
 void	print_escaped_char(char c)
 {
 	char	char_to_print;
+	char	backslash;
 
-	write(1, "\\", 1);
+	backslash = 92;
+	write(1, &backslash, 1);
 	char_to_print = int_to_hex_char((0 + c) / 16);
 	write(1, &char_to_print, 1);
 	char_to_print = int_to_hex_char((0 + c) % 16);
