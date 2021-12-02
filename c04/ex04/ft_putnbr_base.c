f80cba60e8d96454ce78f9ba36cf4984e9b0518a
Louis Xu <louis.s.xu@gmail.com>
2021-12-02 22:10:51 +1030

    Add solution

---
+++ c04/ex04/ft_putnbr_base.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 17:29:02 by lxu               #+#    #+#             */
+/*   Updated: 2021/12/02 18:12:22 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	string_okay(char *str)
+{
+	int	i;
+	int	str_len;
+
+	str_len = 0;
+	while (str[str_len])
+	{
+		if (str[str_len] == '-' || str[str_len] == '+')
+			return (-1);
+		i = str_len + 1;
+		while (str[i])
+		{
+			if (str[str_len] == str[i])
+				return (-1);
+			i++;
+		}
+		str_len++;
+	}
+	if (str_len <= 1)
+		return (-1);
+	return (str_len);
+}
+
+int	log_base(int nbr, int base)
+{
+	int	count;
+
+	count = 0;
+	while (nbr)
+	{
+		count++;
+		nbr = nbr / base;
+	}
+	return (count);
+}
+
+int	exponent(int nbr, int exp)
+{
+	int	result;
+
+	result = 1;
+	while (exp)
+	{
+		result = result * nbr;
+		exp--;
+	}
+	return (result);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int			nbr_len;
+	int			radix_size;
+	int			digit_val;
+	long int	nbr_l;
+
+	if (nbr == 0)
+		write(1, "0", 1);
+	radix_size = string_okay(base);
+	if (radix_size == -1)
+		return ;
+	nbr_len = log_base(nbr, radix_size);
+	if (nbr < 0)
+	{
+		nbr_l = (-1) * (long int)nbr;
+		write(1, "-", 1);
+	}
+	else
+		nbr_l = (long int)nbr;
+	while (nbr_len)
+	{
+		digit_val = (nbr_l / exponent(radix_size, (nbr_len - 1))) % radix_size;
+		write(1, &base[digit_val], 1);
+		nbr_len--;
+	}
+}
---
+++ c04/ex05/ft_atoi_base.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 17:56:16 by lxu               #+#    #+#             */
+/*   Updated: 2021/12/02 22:03:22 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_whitespace(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\v' || \
+			c == '\f' || c == '\r' || c == ' ')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	check_base(char *base)
+{
+	int	size;
+	int	i;
+
+	size = 0;
+	while (base[size])
+	{
+		if (base[size] == '-' || base[size] == '+')
+			return (-1);
+		if (is_whitespace(base[size]))
+			return (-1);
+		i = size + 1;
+		while (base[i])
+		{
+			if (base[size] == base[i])
+				return (-1);
+			i++;
+		}
+		size++;
+	}
+	if (size <= 1)
+		return (-1);
+	return (size);
+}
+
+int	char_to_val(char c, char *base)
+{
+	int	count;
+
+	count = 0;
+	while (base[count])
+	{
+		if (base[count] == c)
+		{
+			return (count);
+		}
+		count++;
+	}
+	return (-1);
+}
+
+int	core_atoi_base(char *str, char *base, int radix)
+{
+	int	digit;
+	int	value;
+
+	value = 0;
+	while (*str)
+	{
+		digit = char_to_val(*str, base);
+		if (digit < 0)
+		{
+			return (value);
+		}
+		value = value * radix;
+		value = value + digit;
+		str++;
+	}
+	return (value);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	radix;
+	int	i;
+	int	is_positive;
+
+	radix = check_base(base);
+	i = 0;
+	is_positive = 1;
+	if (radix == -1)
+		return (0);
+	while (str[i] && is_whitespace(str[i]))
+		i++;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			is_positive = is_positive * (-1);
+		i++;
+	}
+	if (str[i])
+		return (core_atoi_base(&str[i], base, radix) * is_positive);
+	return (0);
+}
+
+// can start with arbitrary amount of whitespace
+// can have arbitrary number of +/- signs
+// can have any number of numbers in the base
+// read until string stops following the rules
+//
+//don't handle over/underflow
