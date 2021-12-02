bf3756b78483905da9ad64dd0a60e79b5433b459
Louis Xu <louis.s.xu@gmail.com>
2021-12-02 22:08:03 +1030

    Fix: Handle MIN_INT (prev out of range)

--- c04-dev/ex02/ft_putnbr.c
+++ c04-dev/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 23:03:19 by lxu               #+#    #+#             */
-/*   Updated: 2021/12/01 23:05:37 by lxu              ###   ########.fr       */
+/*   Updated: 2021/12/02 22:07:44 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 void	ft_putnbr(int nb)
 {
-	long int	size;
+	int			size;
 	char		result[10];
+	long int	nb_l;
 
 	if (nb == 0)
 		write(1, "0", 1);
+	nb_l = nb;
 	if (nb < 0)
 	{
 		write(1, "-", 1);
-		nb = nb * -1;
+		nb_l = (long int)nb * -1;
 	}
 	size = 0;
-	while (nb)
+	while (nb_l)
 	{
-		result[size] = '0' + (nb % 10);
-		nb = nb / 10;
+		result[size] = '0' + (nb_l % 10);
+		nb_l = nb_l / 10;
 		size++;
 	}	
 	while (--size >= 0)
