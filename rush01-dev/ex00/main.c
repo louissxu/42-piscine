7da5beb7e22e69ccfede22e9473bffe9b57fb3d9
Louis Xu <louis.s.xu@gmail.com>
2021-12-05 18:48:29 +1030

    Fix: Norm errors

--- rush01-dev/ex00/main.c
+++ rush01-dev/ex00/main.c
@@ -1,21 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 18:47:18 by lxu               #+#    #+#             */
+/*   Updated: 2021/12/05 18:47:56 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "header.h"
 #include <unistd.h>
 #include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	int return_value;
-	
 	if (argc != 2)
 	{
 		write(2, "Usage ./solve_puzzle \"<constraints\"", 20);
 		return (1);
 	}
-
-	printf("\n\n================\nSTARTING\n================\n");
-	printf("The constraint string is %s\n", argv[1]);
-	//return_value = bf_solve_puzzle(argv[1]);
-	return_value = bt_solve_puzzle(argv[1]);
-	printf("The final return value is %d\n", return_value);
-	return (return_value);
+	bt_solve_puzzle(argv[1]);
+	return (0);
 }
