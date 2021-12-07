02ddaea03cee6f9878948ec1a8646db53e518d5b
Louis Xu <louis.s.xu@gmail.com>
2021-12-07 15:43:28 +1030

    Added extra texts

--- c03-dev/ex00/test_ft_strcmp.c
+++ c03-dev/ex00/test_ft_strcmp.c
@@ -11,6 +11,11 @@ int main(void)
 	printf("Comparing \"123\" and \"1234\": %d\n", strcmp("123", "1234"));
 	printf("Comparing \"321\" and \"123\": %d\n", strcmp("321", "123"));
 	printf("Comparing \"!@#\" and \"123\": %d\n", strcmp("!@#", "123"));
+	printf("Comparing \"abcdef\" and \"abcdefghijklb\": %d\n", strcmp("abcdef", "abcdefghijklb"));
+	char *s1 = "hello";
+	char *s2 = "world";
+	printf("Comparing \"%s\" and \"%s\": %d\n", s1, s2, strcmp(s1, s2));
+	
 
 	printf("-------------------");
 	printf("Using ft_strcmp\n");
@@ -19,4 +24,5 @@ int main(void)
 	printf("Comparing \"123\" and \"1234\": %d\n", ft_strcmp("123", "1234"));
 	printf("Comparing \"321\" and \"123\": %d\n", ft_strcmp("321", "123"));
 	printf("Comparing \"!@#\" and \"123\": %d\n", ft_strcmp("!@#", "123"));
+	printf("Comparing \"%s\" and \"%s\": %d\n", s1, s2, ft_strcmp(s1, s2));
 }
