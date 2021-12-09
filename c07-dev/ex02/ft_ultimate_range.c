7d1e7e9a0bbefac71dd87312eee0724f1402fe74
Louis Xu <louis.s.xu@gmail.com>
2021-12-09 12:08:26 +1030

    Fix: Norm errors

--- c07-dev/ex02/ft_ultimate_range.c
+++ c07-dev/ex02/ft_ultimate_range.c
@@ -6,24 +6,22 @@
 /*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 12:07:41 by lxu               #+#    #+#             */
-/*   Updated: 2021/12/08 12:58:03 by lxu              ###   ########.fr       */
+/*   Updated: 2021/12/09 12:00:01 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-
-#include <stdio.h>
 int	ft_ultimate_range(int **range, int min, int max)
 {
-	int i;
+	int	i;
 
 	if (min >= max)
 	{
 		*range = NULL;
-		return 0;
+		return (0);
 	}
-	*range = (int*)malloc((max - min) * sizeof(int));
+	*range = (int *)malloc((max - min) * sizeof(int));
 	i = 0;
 	while (i < max - min)
 	{
