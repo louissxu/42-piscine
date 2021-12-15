26820d99a0f63ee50c5615f7df60d1d8730ad47e
Louis Xu <louis.s.xu@gmail.com>
2021-12-15 15:27:32 +1030

    Add tests to check the other library functions

--- c09-dev/ex00/main.c
+++ c09-dev/ex00/main.c
@@ -1,6 +1,22 @@
 void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
+
+#include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
 	ft_putchar('a');
+	ft_putstr("Testing putstr");
+	printf("Strlen of hello is: %d\n", ft_strlen("hello"));
+	printf("ft_strcmp of hello and world is :%d\n", ft_strcmp("hello", "world"));
+	printf("strcmp of hello and world is %d\n", strcmp("hello", "world"));
+	int a = 5;
+	int b = 7;
+	printf("a: %d, b: %d\n", a, b);
+	ft_swap(&a, &b);
+	printf("a: %d, b: %d\n", a, b);
 }
